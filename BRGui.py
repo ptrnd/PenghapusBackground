95db35bbfadf2166627feadb9617619e27d3e209
isak <muhammadisak.rizki@gmail.com>
2021-06-04 10:02:19 +0700

    ubah backend

--- BRGui.py
+++ BRGui.py
@@ -1,18 +1,25 @@
 import tkinter as tk
 import matplotlib as mp
 import numpy as np
-
-from tkinter.filedialog import askopenfilename
 from tkinter import ttk
 
+class VisualizeEventListener:
+    def btn_file(self, event: tk.Event):
+        pass
+
+    def btn_save(self, event: tk.Event):
+        pass
 
 class Visualize:
-
-    def __init__(self):
+    def __init__(self, backend : VisualizeEventListener):
         self.window = tk.Tk()
         self.window.geometry('800x600')
         self.window.title('Background Remover')
+
+        self.backend = backend
+
         self.init_widgets()
+        self.__bind_events()
 
     def init_widgets(self):
         self.frame_satu = tk.Frame(master=self.window, borderwidth=1)  # , relief=tk.RAISED)
@@ -23,27 +30,40 @@ class Visualize:
         self.frame_dua = tk.Frame(master=self.window, borderwidth=1)  # , relief=tk.RAISED)
         self.btn_file = tk.Button(master=self.frame_dua, text='Pilih File', width=10)
         self.btn_file.pack(pady=3)
-        self.btn_file.bind('<Button-1>', self.on_click_btn_file)
         self.frame_dua.grid(row=0, column=1, sticky='n')
 
         self.frame_tiga = tk.Frame(master=self.window, borderwidth=1)  # , relief=tk.RAISED)
-        self.lbl_file = tk.Label(master=self.frame_tiga, text='nama file', width=10)
+        self.lbl_file = tk.Label(master=self.frame_tiga, text='nama file', width=100)
         self.lbl_file.pack(pady=3)
-        self.frame_tiga.grid(row=0, column=2, sticky='ne', width=100)
+        self.frame_tiga.grid(row=0, column=2, sticky='ne')
 
+        #Before
         self.frame_empat = tk.Frame(master=self.window, borderwidth=1)
         self.lbl_before = tk.Label(master=self.frame_empat, text='Before', width=10)
         self.lbl_before.pack(pady=3)
+        
+        self.canvas_before = tk.Canvas(master=self.frame_empat)
+        self.canvas_before.pack(pady=3)
+        self.imgBefore = tk.PhotoImage(file="doraemon hitam.png")
+        self.canvas_before.create_image(100,100,image=self.imgBefore)
+
         self.frame_empat.grid(row=1, columnspan=3, sticky='nsew')
 
+        #After
         self.frame_lima = tk.Frame(master=self.window, borderwidth=1)
         self.lbl_after = tk.Label(master=self.frame_lima, text='After', width=10)
         self.lbl_after.pack(pady=3)
+
+        self.canvas_after = tk.Canvas(master=self.frame_lima)
+        self.canvas_after.pack(pady=3)
+        self.imgAfter = tk.PhotoImage(file="doraemon hitam.png")
+        self.canvas_after.create_image(100,100,image=self.imgAfter)
+
         self.frame_lima.grid(row=2, columnspan=3, sticky='s')
 
-    def on_click_btn_file(self, event: tk.Event):
-        filename = askopenfilename()
-        self.lbl_file['text'] = filename
+    def __bind_events(self):
+        self.btn_file.bind('<Button-1>', self.backend.on_click_btn_file)
+
 
     def show(self):
         self.window.mainloop()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from BRGui import Visualize
-
+from backend import Backend
 
 class Main:
     @staticmethod
     def main():
-        main_window = Visualize()
+        bcEnd = Backend()
+        main_window = Visualize(bcEnd)
+        bcEnd.window = main_window
         main_window.show()
 
 
