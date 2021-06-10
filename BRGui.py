db085c0255aff7504d0896df6b9fd93f7ddb449e
isak <muhammadisak.rizki@gmail.com>
2021-06-11 04:17:29 +0700

    finish

--- BRGui.py
+++ BRGui.py
@@ -11,6 +11,9 @@ class VisualizeEventListener:
     def btn_save(self, event: tk.Event):
         pass
 
+    def btn_process(self, event: tk.Event):
+        pass
+
 class Visualize:
     def __init__(self, backend : VisualizeEventListener):
         self.window = tk.Tk()
@@ -23,62 +26,106 @@ class Visualize:
         self.__bind_events()
 
     def init_widgets(self):
-        # label perintah memasukkan gambar
-        self.frame_satu = tk.Frame(master=self.window, borderwidth=1)  # , relief=tk.RAISED)
-        self.lbl_input = tk.Label(master=self.frame_satu, text='Masukkan gambar jpg : ')
-        self.lbl_input.pack(pady=3)
-        self.frame_satu.grid(row=0, column=0, sticky='nw')
-
+        
         # tombol memasukkan gambar
-        self.frame_dua = tk.Frame(master=self.window, borderwidth=1)  # , relief=tk.RAISED)
-        self.btn_file = tk.Button(master=self.frame_dua, text='Pilih File', width=10)
-        self.btn_file.pack(pady=3)
-        self.frame_dua.grid(row=0, column=1, sticky='n')
-        # self.btn_file.bind('<Button-1>', self.backend.on_click_btn_file)
+        self.frame_input_img = tk.Frame(master=self.window, borderwidth=1)  # , relief=tk.RAISED)
+        self.lbl_input = tk.Label(master=self.frame_input_img, text='Masukkan gambar jpg : ')
+        self.lbl_input.pack(pady=1, anchor="w")
+        
+        self.btn_file = tk.Button(master=self.frame_input_img, text='Pilih File', width=10)
+        self.btn_file.pack(pady=1, anchor="w", side='left')
 
-        # label nama file yang dimasukkan
-        self.frame_tiga = tk.Frame(master=self.window, borderwidth=1)  # , relief=tk.RAISED)
-        self.lbl_file = tk.Label(master=self.frame_tiga, text='nama file', width=100)
-        self.lbl_file.pack(pady=3)
-        self.frame_tiga.grid(row=0, column=2, sticky='ne')
+        self.lbl_file = tk.Label(master=self.frame_input_img, text='nama file')
+        self.lbl_file.pack(pady=1, anchor="w")
+        self.frame_input_img.grid(row=0, column=0, sticky='w')
 
-        # frame gambar
+
+        # Setting warna BG
+        self.frame_input_bg = tk.Frame(master=self.window, borderwidth=1)  # , relief=tk.RAISED)
+        
+        self.lbl_red = tk.Label(master=self.frame_input_bg, text='Red : ')
+        self.lbl_red.pack(pady=1,anchor="n", side='left')
+        self.n_red = tk.Entry(master=self.frame_input_bg, width=10)
+        self.n_red.pack(pady=1,side='left')
+
+        self.lbl_green = tk.Label(master=self.frame_input_bg, text='Green : ')
+        self.lbl_green.pack(pady=1,anchor="n", side='left')
+        self.n_green = tk.Entry(master=self.frame_input_bg, width=10)
+        self.n_green.pack(pady=1,side='left')
+
+        self.lbl_blue = tk.Label(master=self.frame_input_bg, text='Blue : ')
+        self.lbl_blue.pack(pady=1,anchor="n", side='left')
+        self.n_blue = tk.Entry(master=self.frame_input_bg, width=10)
+        self.n_blue.pack(pady=1)
+
+        self.n_blue.insert(tk.END, "0")
+        self.n_red.insert(tk.END, "0")
+        self.n_green.insert(tk.END, "0")
+
+        self.frame_input_bg.grid(row=0, column=1, sticky='e')
+        
+        #btn Proses
+        self.frame_setting = tk.Frame(master=self.window, borderwidth=1)
+
+        # self.lbl_max = tk.Label(master=self.frame_setting, text='Max : ')
+        # self.lbl_max.pack(pady=1,anchor="n", side='left')
+        # self.n_max = tk.Entry(master=self.frame_setting, width=10)
+        # self.n_max.pack(pady=1, side='left')
+
+        # self.lbl_min = tk.Label(master=self.frame_setting, text='Min : ')
+        # self.lbl_min.pack(pady=1,anchor="n", side='left')
+        # self.n_min = tk.Entry(master=self.frame_setting, width=10)
+        # self.n_min.pack(pady=1,side='left')
+
+        self.lbl_file_name = tk.Label(master=self.frame_setting, text='Simpan dengan Nama : ')
+        self.lbl_file_name.pack(pady=1,anchor="n", side='left')
+        self.input_name = tk.Entry(master=self.frame_setting, width=20)
+        self.input_name.pack(pady=1,side='left')
+
+        self.input_name.insert(tk.END, "result.png")
+
+        self.btn_process = tk.Button(master=self.frame_setting, text='Proses and Save')
+        self.btn_process.pack(padx=10)
+        self.frame_setting.grid(row=1, column=0, sticky='e', pady=3,  columnspan=2)
+
+
         self.frame_img = tk.Frame(master=self.window, borderwidth=1)
-        self.frame_img.grid(row=2,column=0, columnspan=4)
+        self.frame_img.grid(row=2,column=0, columnspan=2)
 
         # Before
         # label "sebelum"
-        self.frame_empat = tk.Frame(master=self.frame_img, borderwidth=1)
-        self.lbl_before = tk.Label(master=self.frame_empat, text='Before', width=10)
-        self.lbl_before.pack(pady=0)
+        self.frame_before = tk.Frame(master=self.frame_img, borderwidth=1)
+        self.lbl_before = tk.Label(master=self.frame_before, text='Before', width=10)
+        self.lbl_before.pack(pady=1)
 
         # gambar sebelum backgroundnya dihapus
-        self.canvas_before = tk.Canvas(master=self.frame_empat)
-        self.canvas_before.pack(side=tk.LEFT)
+        self.canvas_before = tk.Canvas(master=self.frame_before)
+        self.canvas_before.pack(side='top')
         self.imgBefore = tk.PhotoImage(file="init.png")
         self.imgBefore = self.imgBefore.subsample(2)
-        self.canvas_before.create_image(100,100,image=self.imgBefore)
+        self.canvas_before.create_image(200,100,image=self.imgBefore)
 
-        self.frame_empat.grid(row=0, column=0, sticky='w')
+        self.frame_before.grid(row=0, column=0, sticky='n')
 
         #After
         # label "setelah"
-        self.frame_lima = tk.Frame(master=self.frame_img, borderwidth=1)
-        self.lbl_after = tk.Label(master=self.frame_lima, text='After', width=10)
-        self.lbl_after.pack(pady=0)
+        self.frame_after = tk.Frame(master=self.frame_img, borderwidth=1)
+        self.lbl_after = tk.Label(master=self.frame_after, text='After', width=10)
+        self.lbl_after.pack(pady=1)
 
         # gambar setelah backgroundnya dihapus
-        self.canvas_after = tk.Canvas(master=self.frame_lima)
-        self.canvas_after.pack(side=tk.RIGHT)
+        self.canvas_after = tk.Canvas(master=self.frame_after)
+        self.canvas_after.pack()
         self.imgAfter = tk.PhotoImage(file="init.png")
         self.imgAfter = self.imgAfter.subsample(2)
-        self.canvas_after.create_image(100,100,image=self.imgAfter)
-        self.frame_lima.grid(row=0, column=1, sticky='s')
+        self.canvas_after.create_image(200,100,image=self.imgAfter)
+        self.frame_after.grid(row=0, column=1, sticky='n')
+
 
     # method tombol untuk memasukkan file
     def __bind_events(self):
         self.btn_file.bind('<Button-1>', self.backend.on_click_btn_file)
-        time.sleep(2)
+        self.btn_process.bind('<Button-1>', self.backend.on_click_btn_process)
 
 
     def show(self):
--- backend.py
+++ backend.py
@@ -28,18 +28,20 @@ class Backend(VisualizeEventListener):
         img = cv.imread(file, cv.IMREAD_UNCHANGED)
         original = img.copy()
 
-        # buat edge conture (garis di samping gambar)
-        ed = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        edges = cv.GaussianBlur(img, (21, 51), 4)
-        edges = cv.cvtColor(edges, cv.COLOR_BGR2GRAY)
-        edges = cv.Canny(edges, 6, 6)
+        
 
-        # memperbesar tebal garis
+        ed = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        edges = cv.GaussianBlur(img, (3, 3), 0)
+        edges = cv.cvtColor(edges, cv.COLOR_BGR2GRAY)
+
+        l, thresh_im = cv.threshold(edges, 0, 255, cv.THRESH_BINARY  + cv.THRESH_OTSU)
+        u = 0.5*l
+        edges = cv.Canny(edges, l, u)
+
         _, thresh = cv.threshold(edges, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
         kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
         mask = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel, iterations=4)
 
-        # mengisi area dalam
         data =   mask.tolist()
         for i in  range(len(data)):
             for j in  range(len(data[i])):
@@ -56,51 +58,44 @@ class Backend(VisualizeEventListener):
         image = np.array(data)
         image[image !=  -1] =  255
         image[image ==  -1] =  0
-
         mask = np.array(image, np.uint8)
 
         kernel = np.ones((5,5), np.uint8)
+        mask = cv.morphologyEx(mask, cv.MORPH_ERODE, kernel)
 
-        # memperhalus sudut
-        # closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-        mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
-        mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
-        # mask = cv.morphologyEx(mask, cv.MORPH_ERODE, kernel)
-
-        # clipping (memasukkan gambar di area putih)
         result = cv.bitwise_and(original, original, mask=mask)
         result[mask == 0] = 255
-        # cv.imshow(result)
 
         cv.imwrite('hasil.png', result)
-
         img = Image.open('hasil.png')
         img.convert("RGBA")
         datas = img.getdata()
 
-        # background
         newData = []
         for item in datas:
             if item[0] ==  255  and item[1] ==  255  and item[2] ==  255:
-                newData.append((255, 255, 255, 0))
+                newData.append((int(self.__window.n_red.get()), 
+                                int(self.__window.n_green.get()), 
+                                int(self.__window.n_blue.get()), 255))
             else:
                 newData.append(item)
 
-        # save gambar
+        print(int(self.__window.n_red.get()),self.__window.n_green.get(),int(self.__window.n_blue.get()))
         img.putdata(newData)
-        img.save("img.png", "PNG")
+        img.save("result/"+self.__window.input_name.get(), "PNG")
 
-        self.__window.imgAfter = tk.PhotoImage(file="img.png")
-        self.__window.imgAfter = self.__window.imgAfter.subsample(3)
+        self.__window.imgAfter = tk.PhotoImage(file="result/"+self.__window.input_name.get())
+        self.__window.imgAfter = self.__window.imgAfter.subsample(2)
         self.__window.canvas_after.create_image(50,10,image=self.__window.imgAfter, anchor = "nw")
 
     def on_click_btn_file(self, event: tk.Event):
         filename = askopenfilename()
         time.sleep(2)
-        # menampilkan gambar asli di bagian "before"
         self.__window.lbl_file['text'] = filename
         self.__window.imgBefore = tk.PhotoImage(file=filename)
-        self.__window.imgBefore = self.__window.imgBefore.subsample(3)
+        self.__window.imgBefore = self.__window.imgBefore.subsample(2)
         self.__window.canvas_before.create_image(50,10,image=self.__window.imgBefore,anchor = "nw")
 
-        self.Remove(file=filename)
+    def on_click_btn_process(self, event: tk.Event):
+        self.Remove(file=self.__window.lbl_file['text'])
+
